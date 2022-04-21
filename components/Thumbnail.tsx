192d1e9a5fda56b30b513b3cbe0242b70a04341e
Tai Dentsu <tai.huynh1@iprospect.com>
2022-04-21 13:32:58 +0700

    add thumbnail category

---
+++ components/Thumbnail.tsx
@@ -0,0 +1,20 @@
+import Image from 'next/image'
+import { Movie } from '../models'
+
+export interface ThumbnailProps {
+  movie: Movie
+}
+
+export default function Thumbnail({ movie }: ThumbnailProps) {
+  return (
+    <div className="relative h-28 min-w-[180px] cursor-pointer transition duration-200 ease-out md:h-36 md:min-w-[260px] md:hover:scale-105">
+      <Image
+        src={`https://image.tmdb.org/t/p/w500${
+          movie.backdrop_path || movie.poster_path
+        }`}
+        className="rounded-sm object-cover md:rounded"
+        layout="fill"
+      />
+    </div>
+  )
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -18,7 +18,7 @@ const Home = ({
   trendingNow,
 }: MovieList) => {
   return (
-    <div className="relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh]">
+    <div className="relative h-screen bg-gradient-to-b lg:h-[140vh]">
       <Head>
         <title>Netflix</title>
         <link rel="icon" href="/favicon.ico" />
@@ -27,7 +27,7 @@ const Home = ({
 
       <main className="relative pl-4 pb-24 lg:space-y-24 lg:pl-16">
         <Banner netflixOriginals={netflixOriginals} />
-        <section>
+        <section className="md:space-y-24">
           <Category title="Trending Now" movies={trendingNow} />
           <Category title="Top Rated" movies={topRated} />
           <Category title="Action Thrillers" movies={actionMovies} />
